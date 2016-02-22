f6430915ef385b6d26304378a1aaaa1a7f5cc83b
LuisalopezS <jcatuestav@uqvirtual.edu.co>
2016-02-22 01:00:14 -0500

    version modificada para la rama dos

--- lab1.c
+++ lab1.c
@@ -4,13 +4,13 @@
 int main ()
 {
 int i=0,v[5]={0,1, 2, 3, 4};
-int x=5+5;
+int x=5+3;
 
 while (i<5)
 {  
 printf("imprime %d \n ",v[i]);
 i=i+1;
 }
-printf(" X: %d \n",x );
+printf(" cambio en la rama dos X : %d \n",x );
 return (0);
 }
